3af1f2620f632f65a6280e7735771ec4cd50cf80
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-28 17:03:36 +0530

    Add setup of frontend

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+
+function App() {
+
+ 
+  return (
+    <>
+     <h1>Welcome to the docker project</h1>
+     <h2>data</h2>
+    </>
+  )
+}
+
+export default App
